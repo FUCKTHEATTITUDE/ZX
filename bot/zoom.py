93d526cd2d86f3cae93bb3a486d83097b83e0d46
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-02 11:17:00 +0530

    Update zoom.py

--- bot/zoom.py
+++ bot/zoom.py
@@ -1,13 +1,13 @@
 import logging
 from config import Config
+import warnings
+import threading
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from bot import updater, browser, restricted
+from selenium.webdriver.support import expected_conditions as ec
+from bot import updater,driver , restricted
 from telegram.ext import run_async
 from telegram import ChatAction
 import os
@@ -17,99 +17,130 @@ from os import execl
 from sys import executable
 
 userId = Config.USERID
+MUTEX = threading.Lock()
+proxylist = [
+    "192.99.101.142:7497",
+    "198.50.198.93:3128",
+    "52.188.106.163:3128",
+    "20.84.57.125:3128",
+    "172.104.13.32:7497",
+    "172.104.14.65:7497",
+   "165.225.220.241:10605",
+    "165.225.208.84:10605",
+    "165.225.39.90:10605",
+    "165.225.208.243:10012",
+    "172.104.20.199:7497",
+    "165.225.220.251:80",
+    "34.110.251.255:80",
+    "159.89.49.172:7497",
+    "165.225.208.178:80",
+    "205.251.66.56:7497",
+    "139.177.203.215:3128",
+    "64.235.204.107:3128",
+    "165.225.38.68:10605",
+    "165.225.56.49:10605",
+    "136.226.75.13:10605",
+    "136.226.75.35:10605",
+    "165.225.56.50:10605",
+    "165.225.56.127:10605",
+    "208.52.166.96:5555",
+    "104.129.194.159:443",
+    "104.129.194.161:443",
+    "165.225.8.78:10458",
+    "5.161.93.53:1080",
+    "165.225.8.100:10605",
+]
+
+name = [
+'David Asir',
+'Mohammed UAE',
+'Victor Sam',
+'SENTHILKUMAR DUBAI',
+'Tamilarasan',]
+def sync_print(text):
+    with MUTEX:
+        print(text)
+        
+def get_driver(proxy):
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.74 Safari/537.36"
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_experimental_option("detach", True)
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-infobars")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--use-fake-device-for-media-stream")
+    options.add_argument("--start-maximized")
+    if proxy is not None:
+        options.add_argument(f"--proxy-server={proxy}")      
+def driver_wait(driver, locator, by, secs=1, condition=ec.element_to_be_clickable):
+    wait = WebDriverWait(driver=driver, timeout=secs)
+    element = wait.until(condition((by, locator)))
+    return element
 
 def joinZoom(context, url_meet, passStr):
 
     def students(context):
         print("Running Student Check")
 
-        browser.find_element_by_xpath('//*[@id="wc-container-left"]/div[4]/div/div/div/div[1]').click()
-        number = WebDriverWait(browser, 2400).until(EC.presence_of_element_located((By.XPATH, '//*[@id="wc-footer"]/div/div[2]/button[1]/div/div/span'))).text
+        driver.find_element_by_xpath('//*[@id="wc-container-left"]/div[4]/div/div/div/div[1]').click()
+        number = WebDriverWait(driver, 2400).until(EC.presence_of_element_located((By.XPATH, '//*[@id="wc-footer"]/div/div[2]/button[1]/div/div/span'))).text
         print(number)
         if(int(number) <10):
             context.bot.send_message(chat_id=userId, text="Your Class has ended!")
-            browser.quit()
+            driver.quit()
             execl(executable, executable, "chromium.py")
     try:
-        name = "Vansh Santoshi"
-        browser.get('https://zoom.us')
-        browser.get('https://zoom.us/wc/join/'+ url_meet)
-        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#inputname"))).click()
-        for i in range(0, 20):
-            browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys(Keys.BACK_SPACE)
-        browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys(name)
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#joinBtn"))).click()
-        print("Clicked on join button")
+        sync_print(f"{name} started!")
+        driver = get_driver(proxy)
+        driver.get(f'https://zoom.us/wc/join/'+meetingcode)
+        time.sleep(3)
+        inp = driver.find_element(By.ID, 'inputname')
+        time.sleep(1)
+        inp.send_keys(f"{user}")
+        btn2 = driver.find_element(By.ID, 'joinBtn')
+        btn2.click()
+        time.sleep(2)
+        inp2 = driver.find_element(By.ID, 'inputpasscode')
+        time.sleep(1)
+        inp2.send_keys(passStr)
+        btn3 = driver.find_element(By.ID, 'joinBtn')
+        time.sleep(1)
+        btn3.click()
+        sync_print(f"{name} sleep for {wait_time} seconds ...")
+        time.sleep(wait_time)
+        sync_print(f"{name} ended!")
 
         try:
-            logout = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, "SIGN IN")))
-            print("User is logged out. Logging in them again")
-            browser.get("https://zoom.us/google_oauth_signin")
-            WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".wLBAL"))).click()
-            time.sleep(10)
-            browser.get('https://zoom.us')
-            browser.get('https://zoom.us/wc/join/'+ url_meet)
-            WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#inputname"))).click()
-            for i in range(0, 20):
-                browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys(Keys.BACK_SPACE)
-            browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys(name)
-            WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#joinBtn"))).click()
-            print("Clicked on join button after logging in")
+            continue:
 
         except NoSuchElementException:
             print("User is already logged in. Continuing")
         except Exception as e:
             print(e)
             print("Probably, Terms and policies agreement isnt asked for.")
-        try:
-            for button in WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(., 'Continue')]"))):
-                 button.click()
-                 print("Trying to click the continue button")
-        except Exception as e:
-            print(e)
-            print("Is it a good error, or a bad error ? Sore wa.... yami no nakae")
-        try:
-            WebDriverWait(browser, 2400).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="inputpasscode"]'))).send_keys(passStr)
-            print("Entered the code")
-        except:
-            context.bot.send_message("Meeting didn't start. Probably Cancelled")
+        
 
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#joinBtn"))).click()
-        print("Clicked join after entering passcode")
-        time.sleep(15)
-        browser.save_screenshot("ss.png")
+        
+        driver.save_screenshot("ss.png")
         context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
         mid  = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
         os.remove('ss.png')
         context.bot.send_chat_action(chat_id=userId, action=ChatAction.TYPING)
         context.bot.send_message(chat_id=userId, text="Attending you lecture. You can chill :v")
         logging.info("STAAAAPH!!")
-#Join Audio Part
-        try:
-             action = webdriver.ActionChains(browser)
-             action.move_by_offset(100, 100).perform()
-             WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".join-audio-container__btn"))).click()
-             WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".join-audio-by-voip"))).click()
-             WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".join-dialog__close"))).click()
-        except Exception as e:
-             print(e)
-             print("Maybe the dialog got closed by itself, or the website layout has changed ?")
-#########
-#        context.bot.delete_message(chat_id=userId ,message_id = mid)
-#        browser.save_screenshot("ss.png")
-#        context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
-#        mid = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
-#        os.remove('ss.png')
-
-        WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="voip-tab"]/div/button'))).click()
-
-        WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="wc-footer"]/div/div[2]/button[1]'))).click()
-
-        WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="wc-container-right"]/div/div[2]/div/button[2]'))).click()
 
 
-    except Exception as e:
-        browser.save_screenshot("ss.png")
+        
+except Exception as e:
+        driver.save_screenshot("ss.png")
         context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
         mid  = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
         os.remove('ss.png')
