200cfa502199c26e040003be12f6f2317c6e93b9
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-02 07:49:43 +0530

    Update RunLocallyInit.py

--- bot/RunLocallyInit.py
+++ bot/RunLocallyInit.py
@@ -2,21 +2,17 @@ import os
 from telegram.ext import Updater
 from config import Config
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.Chrome.options import Options as ChromeOptions
+from selenium.webdriver.Chrome.Chrome_profile import ChromeProfile
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 try:
-	BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-	print(BASE_DIR)
-	s = BASE_DIR + '/Profile/'
-	print (s)
+	
 	updater = Updater(token = Config.BOT_TOKEN, use_context=True)
 	dp = updater.dispatcher
-	options = webdriver.FirefoxOptions()
+	options = webdriver.ChromeOptions()
 	options.add_argument("-profile")
-	options.add_argument(s)
-	#profile = FirefoxProfile()
+	#profile = ChromeProfile()
 	#profile.set_preference("dom.webdriver.enabled", False)
 	#profile.set_preference('useAutomationExtension', False)
 	#profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/60.0.3112.50 Safari/537.36")
@@ -26,8 +22,8 @@ try:
 	#options.add_argument("--headless")
 	#options.add_argument("--window-size=1200,800")
 	options.add_argument("--disable-blink-features=AutomationControlled")
-	#browser = webdriver.Firefox(options=options, firefox_profile=profile, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
-	browser = webdriver.Firefox(options=options, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
+	#browser = webdriver.Chrome(options=options, firefox_profile=profile, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
+	browser = webdriver.Chrome(options=options, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
 	#browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 except Exception as e:
 	print(e)
